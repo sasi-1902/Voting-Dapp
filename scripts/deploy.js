b6b3a76111146b82889728cbc5cf131816596243
Sasi Deepika Eluri <149556178+sasi-1902@users.noreply.github.com>
2024-07-22 21:28:19 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,10 @@
 async function main() {
   const Voting = await ethers.getContractFactory("Voting");
 
+  const durationInSeconds = 3 * 7 * 24 * 60 * 60;
+
   // Start deployment, returning a promise that resolves to a contract object
-  const voting = await Voting.deploy(["Euno", "Margret", "Henry", "Shaun"], 5760); // 4 days in minutes
+  const voting = await Voting.deploy(["Euno", "Margret", "Henry", "Shaun"], durationInSeconds);
   console.log("Contract address:", voting.address);
 }
 
